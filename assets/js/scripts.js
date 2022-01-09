28abd2b5d034d08141aa5ba7fd4fc649f4ff7cfe
Georges <georgeslyangus@gmail.com>
2022-01-09 15:05:11 -0300

    projeto final

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,7 +7,20 @@ function addEvent() {
   var elementCreatedFirstChild = document.createElement("Input");
   elementCreated.appendChild(elementCreatedFirstChild);
   elementCreatedFirstChild.setAttribute("type", "checkbox");
+  elementCreatedFirstChild.setAttribute("class", "checkbox-button");
+  elementCreatedFirstChild.addEventListener("click", check);
   var elementCreatedLastChild = document.createElement("Span");
   elementCreated.appendChild(elementCreatedLastChild);
   elementCreatedLastChild.innerHTML = clientInput.value;
+}
+
+function check() {
+  var checkboxButton = document.getElementsByClassName("checkbox-button");
+  for (let i = 0; i < checkboxButton.length; i++) {
+    if (checkboxButton[i].checked === true) {
+      checkboxButton[i].nextSibling.setAttribute("class", "line-through");
+    } else {
+      checkboxButton[i].nextSibling.removeAttribute("class");
+    }
+  }
 }
